d31c0b8ea9cb555a15eea26c3e7c56ef2cc312c9
cuongeuro226 <cuongeuro226@users.noreply.github.com>
2015-12-22 17:07:33 +0700

    add and update Bll

---
+++ code/QuanLyCuaHangLinhKienMayTinh/BLL/Warehouse/BllDistributor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Warehouse;
+
+namespace BLL.Warehouse
+{
+    public class BllDistributor
+    {
+        private DalDistributor _dalDistributor;
+
+        public BllDistributor()
+        {
+            _dalDistributor = new DalDistributor();
+        }
+        public DataTable GetDistributorList()
+        {
+            return _dalDistributor.GetDistributorList();
+        }
+    }
+}
---
+++ code/QuanLyCuaHangLinhKienMayTinh/BLL/Warehouse/BllWarehouseBill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL.Warehouse
+{
+    public class BllWarehouseBill
+    {
+        private DalWarehouseBill _dalWarehouse;
+
+        public BllWarehouseBill()
+        {
+            _dalWarehouse = new DalWarehouseBill();
+        }
+
+        public DataTable GetWarehouseBillList()
+        {
+            return _dalWarehouse.GetWarehouseBillList();
+        }
+    }
+}
