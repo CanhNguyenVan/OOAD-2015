8e6c43863bd2b605eae6d4c5bfcefcb424eea976
thaoho21294 <thaoho21294@gmail.com>
2016-01-09 22:21:12 +0700

    Add data CHITIETHOADON

--- code/SQL/QLBH_CuaHangBanMayTinhVaLinhKien.sql
+++ code/SQL/QLBH_CuaHangBanMayTinhVaLinhKien.sql
@@ -13,7 +13,7 @@ CREATE TABLE KHACHHANG
 (
 	MaKH nvarchar(10) primary key NOT NULL,
 	TenKH nvarchar(100) NOT NULL,
-	DiaChi nvarchar(1000)  NULL,
+	DiaChi nvarchar(4000)  NULL,
 	SoDT nvarchar(20) NOT NULL,
 	CMND nvarchar(20) NOT NULL,
 	Email nvarchar(50) NULL,
@@ -281,55 +281,71 @@ CREATE TABLE THAMSO
 	TenTS nvarchar(100) not null,
 	GiaTri int not null,
 )
-insert into SANPHAM values('SP1', 'USB Tosiba',N'Phụ kiện', 60, 100000, 150000, 20, N'cái', 'Hàng Trung Quốc');
-insert into SANPHAM values('SP2', 'USB Kington 8G',N'Phụ Kiện', 60, 600000, 85000, 20, N'cái', 'Hàng dổm 100%');
-insert into SANPHAM values('SP3', 'USB Dell 16G US',N'Phụ kiện', 80, 100000, 150000, 20, N'cái', 'Không có gì để ghi chú');
-insert into SANPHAM values('SP4', 'Chuot', N'Phụ kiện', 60,  10000, 60000, 20, N'cái', '');
-insert into SANPHAM values('SP5', 'RAM', N'Phụ kiện', 399000, 60, 500000, 20, N'cái', '');
-insert into SANPHAM values('SP6', 'Man hinh LCD',N'Phụ kiện', 120, 977000, 1500000, 20, N'cái', 'Màng hình lậu');
-insert into SANPHAM values('SP7', 'Máy tính Lenovo', N'Laptop', 11399000, 120, 11500000, 90, N'cái', '');
-insert into SANPHAM values('SP8', 'Lap top Dell 5600',N'laptop', 120, 11000000, 15000000, 10, N'cái', '');
-insert into SANPHAM values('SP9', 'Ipad Air 2', N'Máy tính bảng', 10000000, 60, 19500000, 20, N'cái', '');
-insert into SANPHAM values('SP10', 'Iphone 7s',N'Điện thoại', 100, 12000000, 18000000, 9, N'cái', 'Máy dổm');
-insert into SANPHAM values('SP11', 'SamSum Galaxy',N'Điện thoại', 100, 9000000, 16600000, 12, N'cái', 'Máy vip');
-
-
-INSERT INTO KHACHHANG VALUES('KH001',N'Nguyễn Nghĩa','273145671',N'168,Cộng Hòa, TPHCM','01676857350', null)
-INSERT INTO KHACHHANG VALUES('KH002',N'Võ Thị Bì','273145672',N'Quận9, TPHCM','01676857350', null)
-INSERT INTO KHACHHANG VALUES('KH003',N'Nguyễn Quang Sáng','273145673',N'Quận Thủ Đức, TPHCM','1234567890', null)
-INSERT INTO KHACHHANG VALUES('KH004',N'Nguyễn văn Tèo','273145673',N'Quận Bình Chánh, TP','1234522890', null)
-INSERT INTO KHACHHANG VALUES('KH005',N'Lê Phương Vy','273145675',N'Kim Long,Châu Đức, ','0167906350', null)
-INSERT INTO KHACHHANG VALUES('KH006',N'Ưng Hoàng Phúc','273145676',N'87 Nguyễn Trãi,Quận 1, TPHCH','08906857350', null)
-INSERT INTO KHACHHANG VALUES('KH007',N'Hoàng Hải','273145677',N'90 Lí Thường Kiệt,Quận 1, TPHCM','01623457350', null)
-INSERT INTO KHACHHANG VALUES('KH008',N'Trịnh Công','273145678',N'Mỏ Cày, Cà Mau','01676857350', null)
-INSERT INTO KHACHHANG VALUES('KH009',N'Văn thị Vẽ','273145679',N'32, Lê Lợi,Quận 9, TPHCM','0996857350', null)
-INSERT INTO KHACHHANG VALUES('KH010',N'Hứa Văn Lèo','273145610',N'Ngãi Giao, BR-VT','01676857350', null)
-INSERT INTO KHACHHANG VALUES('KH012',N'Hồ Huyền Trang','273145611',N'234 Xa Lộ Hà Nội, TPHCM','009857380', null)
-INSERT INTO KHACHHANG VALUES('KH013',N'Barack Obama','100078392',N'Hoàn Kiếm, Hà Nội','01670007350', null)
-INSERT INTO KHACHHANG VALUES('KH014',N'Tô Minh Đức','397456922',N'Thanh Khuê, Đà Nẵng','0976845150', null)
-INSERT INTO KHACHHANG VALUES('KH015',N'Nguyễn Du','258932344',N'100 Xa Lộ Hà Nội, TPHCM','01676857350',null)
+--Data for NHANVIEN
+INSERT [dbo].[NHANVIEN] ([MaNV], [Ten], [Tuoi], [CMND], [SDT], [NoiSinh], [NoiTamTru], [TinhTrangHonNhan], [TrinhTrangCongViec], [NgayVaoLam]) VALUES (N'NV1', N'Tr?n Th? Th?m', 29, N'123456789', N'0989765890', N'Hải Phòng', N'Tp HCM', N'Độc Thân', N'Đang làm', CAST(N'2015-09-11 00:00:00' AS SmallDateTime))
+--Data for SANPHAM
+insert into SANPHAM values('SP000001', 'USB Tosiba',N'Phụ kiện', 60, 100000, 150000, 20, N'cái', 'Hàng Trung Quốc');
+insert into SANPHAM values('SP000002', 'USB Kington 8G',N'Phụ Kiện', 60, 600000, 85000, 20, N'cái', 'Hàng dổm 100%');
+insert into SANPHAM values('SP000003', 'USB Dell 16G US',N'Phụ kiện', 80, 100000, 150000, 20, N'cái', 'Không có gì để ghi chú');
+insert into SANPHAM values('SP000004', 'Chuot', N'Phụ kiện', 60,  10000, 60000, 20, N'cái', '');
+insert into SANPHAM values('SP000005', 'RAM', N'Phụ kiện', 399000, 60, 500000, 20, N'cái', '');
+insert into SANPHAM values('SP000006', 'Man hinh LCD',N'Phụ kiện', 120, 977000, 1500000, 20, N'cái', 'Màng hình lậu');
+insert into SANPHAM values('SP000007', 'Máy tính Lenovo', N'Laptop', 11399000, 120, 11500000, 90, N'cái', '');
+insert into SANPHAM values('SP000008', 'Lap top Dell 5600',N'laptop', 120, 11000000, 15000000, 10, N'cái', '');
+insert into SANPHAM values('SP000009', 'Ipad Air 2', N'Máy tính bảng', 10000000, 60, 19500000, 20, N'cái', '');
+insert into SANPHAM values('SP000010', 'Iphone 7s',N'Điện thoại', 100, 12000000, 18000000, 9, N'cái', 'Máy dổm');
+insert into SANPHAM values('SP000011', 'SamSum Galaxy',N'Điện thoại', 100, 9000000, 16600000, 12, N'cái', 'Máy vip');
+--Data for KHACH HANG
+INSERT INTO KHACHHANG VALUES('KH000001',N'Nguyễn Nghĩa',N'168,Cộng Hòa, TPHCM','01676857350','273145671', null)
+INSERT INTO KHACHHANG VALUES('KH000002',N'Võ Thị Bì',N'Quận9, TPHCM','01676857350','273145672', null)
+INSERT INTO KHACHHANG VALUES('KH000003',N'Nguyễn Quang Sáng',N'Quận Thủ Đức, TPHCM','1234567890','273145673', null)
+INSERT INTO KHACHHANG VALUES('KH000004',N'Nguyễn văn Tèo',N'Quận Bình Chánh, TPHCM','1234522890','273145673', null)
+INSERT INTO KHACHHANG VALUES('KH000005',N'Lê Phương Vy',N'Kim Long,Châu Đức,Bà Rịa-Vũng Tàu ','0167906350','273145675', null)
+INSERT INTO KHACHHANG VALUES('KH000006',N'Ưng Hoàng Phúc',N'87 Nguyễn Du, Quận 1, TpHCM','089068573','27314567', null)
+INSERT INTO KHACHHANG VALUES('KH000007',N'Hoàng Hải',N'90 Lý CN, Quận 1, tpHCM','01623457350','273145677', null)
+INSERT INTO KHACHHANG VALUES('KH000008',N'Trịnh Công',N'Mỏ Cày, Cà Mau','01676857350','273145678', null)
+INSERT INTO KHACHHANG VALUES('KH000009',N'Văn thị Vẽ',N'168,Cộng Hòa, TPHCM','0996857350','273145679', null)
+INSERT INTO KHACHHANG VALUES('KH000010',N'Hứa Văn Lèo',N'Ngãi Giao, BR-VT','01676857350','273145610', null)
+INSERT INTO KHACHHANG VALUES('KH000012',N'Hồ Huyền Trang',N'234 Xa Lộ HN, TPHCM','009857380','273145611', null)
+INSERT INTO KHACHHANG VALUES('KH000013',N'Barack Obama',N'Hoàn Kiếm, Thu đô Hà Nội','01670007350','100078392', null)
+INSERT INTO KHACHHANG VALUES('KH000014',N'Tô Minh Đức',N'Thanh Khuê, thành phố Đà Nẵng','0976845150','397456922', null)
+INSERT INTO KHACHHANG VALUES('KH000015',N'Nguyễn Du',N'100 Xa Lộ Hà Nội, TPHCM','01676857350','258932344',null)
 --Data for QUYDINH table
-
 insert into THAMSO values('0', N'Thuế suất', 10);
 set dateformat dmy;
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD1', N'SP10', 1)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD1', N'SP4', 4)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD1', N'SP5', 3)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD2', N'SP10', 1)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD2', N'SP3', 1)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD3', N'SP10', 3)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD3', N'SP11', 2)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD4', N'SP3', 1)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD4', N'SP6', 2)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD5', N'SP10', 7)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD6', N'SP11', 3)
-INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD6', N'SP5', 8)
-INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD1', CAST(N'2015-12-22 00:00:00' AS SmallDateTime), N'KH005', N'NV1', 19560000.0000)
-INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD2', CAST(N'2015-12-24 00:00:00' AS SmallDateTime), N'KH003', N'NV1', 18150000.0000)
-INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD3', CAST(N'2015-12-24 00:00:00' AS SmallDateTime), N'KH002', N'NV1', 105350000.0000)
-INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD4', CAST(N'2015-12-24 00:00:00' AS SmallDateTime), N'KH003', N'NV1', 108500000.0000)
-INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD5', CAST(N'2015-12-24 00:00:00' AS SmallDateTime), N'KH013', N'NV1', 234500000.0000)
-INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD6', CAST(N'2015-12-24 00:00:00' AS SmallDateTime), N'KH014', N'NV1', 288300000.0000)
+--data for CHITETHOADON
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000001', N'SP000001', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000001', N'SP000003', 2)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000002', N'SP000009', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000003', N'SP000010', 2)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000004', N'SP000004', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000004', N'SP000006', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000004', N'SP000011', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000005', N'SP000001', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000005', N'SP000011', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000006', N'SP000005', 2)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000006', N'SP000006', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000006', N'SP000009', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000007', N'SP000004', 2)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000007', N'SP000011', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000008', N'SP000003', 2)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000008', N'SP000005', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000009', N'SP000009', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000009', N'SP000011', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000010', N'SP000004', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000011', N'SP000006', 1)
+INSERT [dbo].[CHITIETHOADON] ([MaHD], [MaSanPham], [SoLuong]) VALUES (N'HD000011', N'SP000008', 1)
+-- Data for HOADON
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000001', CAST(N'2015-12-28 00:00:00' AS SmallDateTime), N'KH000001', N'NV1', 300000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000002', CAST(N'2015-12-28 00:00:00' AS SmallDateTime), N'KH000003', N'NV1', 19500000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000003', CAST(N'2015-12-28 00:00:00' AS SmallDateTime), N'KH000013', N'NV1', 37500000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000004', CAST(N'2015-12-28 00:00:00' AS SmallDateTime), N'KH000009', N'NV1', 55660000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000005', CAST(N'2015-12-28 00:00:00' AS SmallDateTime), N'KH000015', N'NV1', 72410000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000006', CAST(N'2015-12-29 00:00:00' AS SmallDateTime), N'KH000005', N'NV1', 21500000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000007', CAST(N'2015-12-29 00:00:00' AS SmallDateTime), N'KH000008', N'NV1', 38160000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000008', CAST(N'2015-12-29 00:00:00' AS SmallDateTime), N'KH000012', N'NV1', 38810000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000009', CAST(N'2015-12-29 00:00:00' AS SmallDateTime), N'KH000004', N'NV1', 74910000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000010', CAST(N'2016-01-02 00:00:00' AS SmallDateTime), N'KH000005', N'NV1', 60000.0000)
+INSERT [dbo].[HOADON] ([MaHD], [NgayHD], [MaKH], [MaNV], [ThanhTien]) VALUES (N'HD000011', CAST(N'2016-01-02 00:00:00' AS SmallDateTime), N'KH000008', N'NV1', 16500000.0000)
 
-INSERT into THAMSO(TenTS, GiaTri) values(N'Thuế suất', 10)
-select MaSanPham, count(*) from HOADONTRA where MaHD='HD2' group by MaSanPham
+
